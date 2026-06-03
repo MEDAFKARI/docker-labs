edbf59e0d01356eb7c0d9d0c5935b5167d37e8e4
mohammedafkari <mafkari@caf-technologies.com>
2026-06-03 21:57:06 +0100

    freestyling in java images

---
+++ lab-dockerimage/spring-image/tinny-app/src/main/java/com/spring_docker_lab/tinny_app/TinnyAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring_docker_lab.tinny_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinnyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinnyAppApplication.class, args);
+	}
+
+}
---
+++ lab-dockerimage/spring-image/tinny-app/src/test/java/com/spring_docker_lab/tinny_app/TinnyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_docker_lab.tinny_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinnyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
